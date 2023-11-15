7350ce56b66a4c700a687f2abc412a297e96f927
Jugu Tenerat <133909875+tennjugu@users.noreply.github.com>
2023-11-15 16:17:48 +0100

    add search show feature

--- src/navbar.js
+++ src/navbar.js
@@ -1,5 +1,42 @@
+/* eslint-disable no-undef */
+/* eslint-disable consistent-return */
+/* eslint-disable no-use-before-define */
+/* eslint-disable prefer-template */
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-items');
+const searchBtn = document.querySelector('.search-button');
+
+searchBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  getSearchShow();
+});
+
+async function getSearchShow() {
+  try {
+    const searchInput = document.getElementById('search').value;
+    const url = 'https://api.tvmaze.com/';
+    if (searchInput !== '') {
+      const response = await fetch(`${url}search/shows?q=${searchInput}`);
+      const processedSearch = fetchedData(response);
+      return processedSearch;
+    } console.log ('No search input provided.');
+  } catch (error) {
+    console.log (error);
+  }
+}
+
+async function fetchedData(response) {
+  const result = await response.json();
+  if (response.ok) {
+    if (result.length === 0) {
+      console.log('No movies found.');
+    } else {
+      console.log(result);
+    }
+    return result;
+  } console.log(result.Error);
+  throw new Error(result.Error);
+}
 
 function closeMenu() {
   hamburger.classList.remove('active');

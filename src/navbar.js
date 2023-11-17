84da0c347a5a531bb40599bfe188b190a971b96d
Jugu Tenerat <133909875+tennjugu@users.noreply.github.com>
2023-11-17 23:56:32 +0100

    add function to display search show details

--- src/navbar.js
+++ src/navbar.js
@@ -7,9 +7,12 @@ const navMenu = document.querySelector('.nav-items');
 const searchBtn = document.querySelector('.search-button');
 const tvShows = document.querySelectorAll('#tvshows');
 
-searchBtn.addEventListener('click', (event) => {
+searchBtn.addEventListener('click', async (event) => {
+  const content = document.querySelector('.content');
+  content.innerHTML = '';
   event.preventDefault();
-  getSearchShow();
+  const searched = await getSearchShow();
+  seeShowDetails(searched);
 });
 
 tvShows.forEach((tvshownav) => tvshownav.addEventListener('click', (event) => {
@@ -34,16 +37,15 @@ async function getSearchShow() {
 }
 
 async function fetchedData(response) {
-  const result = await response.json();
+  const results = await response.json();
   if (response.ok) {
-    if (result.length === 0) {
-      console.log('No movies found.');
-    } else {
-      console.log(result);
-    }
-    return result;
-  } console.log(result.Error);
-  return result.Error;
+    // console.log(results);
+    const mainResult = results[0]; // return onlt the first item
+    console.log(mainResult);
+    seeShowDetails(mainResult);
+  }
+  console.log(results.Error);
+  return results.Error;
 }
 
 async function getShowsList() {
@@ -58,6 +60,69 @@ async function getShowsList() {
     console.log(error);
     return error;
   }
+}
+
+function seeShowDetails(showDetails) {
+  const content = document.querySelector('.content');
+
+  const fullDetailsContainer = document.createElement('div');
+  fullDetailsContainer.className = 'full-details';
+
+  const imageCard = document.createElement('div');
+  imageCard.className = 'image-card';
+
+  const img = document.createElement('img');
+  img.src = showDetails.show?.image?.medium;
+  img.alt = 'show poster';
+
+  const showInfo = document.createElement('div');
+  showInfo.className = 'show-info';
+
+  const showTitle = document.createElement('h1');
+  showTitle.className = 'showTitle';
+  showTitle.textContent = showDetails?.show?.name;
+
+  const year = document.createElement('h3');
+  year.className = 'year';
+  year.textContent = `Ended: ${showDetails.show.ended}`;
+
+  const genre = document.createElement('h3');
+  genre.className = 'genre';
+  genre.textContent = showDetails.show.genres.join(', ');
+
+  const language = document.createElement('h3');
+  language.className = 'language';
+  language.textContent = `Language: ${showDetails?.show?.language}`;
+
+  const rating = document.createElement('h3');
+  rating.className = 'rating';
+  rating.textContent = `Rating: ${showDetails.show?.rating?.average}/10` || '';
+
+  const showSummary = document.createElement('div');
+  showSummary.className = 'show-summary';
+
+  const plotTitle = document.createElement('h2');
+  plotTitle.className = 'plot';
+  plotTitle.textContent = 'Plot Summary';
+
+  const summary = document.createElement('div');
+  summary.className = 'summary';
+  summary.innerHTML = showDetails.show?.summary;
+
+  showSummary.appendChild(plotTitle);
+  showSummary.appendChild(summary);
+
+  const showInfos = [showTitle, year, genre, rating, showSummary];
+  for (let i = 0; i < showInfos.length; i += 1) {
+    showInfo.appendChild(showInfos[i]);
+  }
+
+  imageCard.appendChild(img);
+  imageCard.appendChild(showInfo);
+
+  fullDetailsContainer.appendChild(imageCard);
+
+  content.appendChild(fullDetailsContainer);
 }
 
 function closeMenu() {

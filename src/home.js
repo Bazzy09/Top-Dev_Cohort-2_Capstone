dbc5367a788a8d111b5b28030ff5188921a27c9d
Ali George <george7ali@gmail.com>
2023-11-18 08:45:47 +0100

    indent the home file

--- src/home.js
+++ src/home.js
@@ -1,84 +1,84 @@
 export function createAppend(content, show) {
-    const searchResultDiv = document.createElement('div');
-    searchResultDiv.classList.add('search-result');
+  const searchResultDiv = document.createElement('div');
+  searchResultDiv.classList.add('search-result');
 
-    const posterImg = document.createElement('img');
-    posterImg.src = show.image ? show.image.medium : './img/placeholder.jpg';
-    posterImg.alt = 'Poster';
-    posterImg.id = 'poster';
+  const posterImg = document.createElement('img');
+  posterImg.src = show.image ? show.image.medium : './img/placeholder.jpg';
+  posterImg.alt = 'Poster';
+  posterImg.id = 'poster';
 
-    const title = document.createElement('h3');
-    title.textContent = show.name;
-    title.classList.add('title');
-    title.id = 'title';
+  const title = document.createElement('h3');
+  title.textContent = show.name;
+  title.classList.add('title');
+  title.id = 'title';
 
-    const likeIcon = document.createElement('img');
-    likeIcon.src = './img/like.png';
-    likeIcon.alt = 'Like';
+  const likeIcon = document.createElement('img');
+  likeIcon.src = './img/like.png';
+  likeIcon.alt = 'Like';
 
-    const likeText = document.createElement('div');
-    likeText.textContent = 'Like';
+  const likeText = document.createElement('div');
+  likeText.textContent = 'Like';
 
-    const likesCounter = document.createElement('div');
-    likesCounter.textContent = `3 likes`;
+  const likesCounter = document.createElement('div');
+  likesCounter.textContent = `3 likes`;
 
-    const commentButton = document.createElement('button');
-    commentButton.textContent = 'Comment';
-    commentButton.classList.add('comment');
+  const commentButton = document.createElement('button');
+  commentButton.textContent = 'Comment';
+  commentButton.classList.add('comment');
 
-    const reservationButton = document.createElement('button');
-    reservationButton.textContent = 'Reservations';
-    reservationButton.classList.add('reservation');
+  const reservationButton = document.createElement('button');
+  reservationButton.textContent = 'Reservations';
+  reservationButton.classList.add('reservation');
 
-    const searchResultPoster = document.createElement('div');
-    searchResultPoster.classList.add('search-result-poster');
-    searchResultPoster.appendChild(posterImg);
+  const searchResultPoster = document.createElement('div');
+  searchResultPoster.classList.add('search-result-poster');
+  searchResultPoster.appendChild(posterImg);
 
-    const searchResultName = document.createElement('div');
-    searchResultName.classList.add('search-result-name');
-    searchResultName.appendChild(title);
+  const searchResultName = document.createElement('div');
+  searchResultName.classList.add('search-result-name');
+  searchResultName.appendChild(title);
 
-    const reactionDiv = document.createElement('div');
-    reactionDiv.classList.add('reaction-action');
+  const reactionDiv = document.createElement('div');
+  reactionDiv.classList.add('reaction-action');
 
-    const reaction = document.createElement('div');
-    reaction.classList.add('likes');
-    reaction.appendChild(likeIcon);
-    reaction.appendChild(likeText);
+  const reaction = document.createElement('div');
+  reaction.classList.add('likes');
+  reaction.appendChild(likeIcon);
+  reaction.appendChild(likeText);
 
-    const likesDiv = document.createElement('div');
-    likesDiv.classList.add('reaction');
-    likesDiv.appendChild(reaction);
-    likesDiv.appendChild(likesCounter);
+  const likesDiv = document.createElement('div');
+  likesDiv.classList.add('reaction');
+  likesDiv.appendChild(reaction);
+  likesDiv.appendChild(likesCounter);
 
-    const actionsDiv = document.createElement('div');
-    actionsDiv.classList.add('actions');
-    actionsDiv.appendChild(commentButton);
-    actionsDiv.appendChild(reservationButton);
+  const actionsDiv = document.createElement('div');
+  actionsDiv.classList.add('actions');
+  actionsDiv.appendChild(commentButton);
+  actionsDiv.appendChild(reservationButton);
 
-    reactionDiv.appendChild(likesDiv);
-    reactionDiv.appendChild(actionsDiv);
+  reactionDiv.appendChild(likesDiv);
+  reactionDiv.appendChild(actionsDiv);
 
-    searchResultDiv.appendChild(searchResultPoster);
-    searchResultDiv.appendChild(searchResultName);
-    searchResultDiv.appendChild(reactionDiv);
+  searchResultDiv.appendChild(searchResultPoster);
+  searchResultDiv.appendChild(searchResultName);
+  searchResultDiv.appendChild(reactionDiv);
 
-    content.appendChild(searchResultDiv);
+  content.appendChild(searchResultDiv);
 }
     
 export async function displayHomeResults() {
-    const content = document.querySelector('#content');
-    const url = 'https://api.tvmaze.com/shows';
-    try {
-      console.log('Fetching TV shows...');
-      const response = await fetch(url);
-      const loadedTvShows = await response.json();
-      (loadedTvShows.slice(0, 15)).forEach(show => {
-        createAppend(content, show)
-    })
-      return loadedTvShows;
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
+  const content = document.querySelector('#content');
+  const url = 'https://api.tvmaze.com/shows';
+  try {
+    console.log('Fetching TV shows...');
+    const response = await fetch(url);
+    const loadedTvShows = await response.json();
+    (loadedTvShows.slice(0, 15)).forEach(show => {
+      createAppend(content, show)
+  })
+    return loadedTvShows;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }

3a53e96c9ed94559d28b28b21dbf364c70b8821a
Ali George <george7ali@gmail.com>
2023-11-18 03:26:45 +0100

    create a div to display image and title(name) of data

--- src/home.js
+++ src/home.js
@@ -2,7 +2,15 @@ export function createAppend(content, show) {
     const searchResultDiv = document.createElement('div');
     searchResultDiv.classList.add('search-result');
 
+    const posterImg = document.createElement('img');
+    posterImg.src = show.image ? show.image.medium : './img/placeholder.jpg';
+    posterImg.alt = 'Poster';
+    posterImg.id = 'poster';
 
+    const title = document.createElement('h3');
+    title.textContent = show.name;
+    title.classList.add('title');
+    title.id = 'title';
 
 
 

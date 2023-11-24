caabd16e630e11c843d2139e7315978b9e12653c
Ali George <george7ali@gmail.com>
2023-11-24 01:37:25 +0100

    write the function to toggle like

--- src/likes.js
+++ src/likes.js
@@ -4,6 +4,14 @@ const counter = document.querySelector('.counter');
 let isLiked = false;
 
 export function toggleLike() {
-
+  isLiked = !isLiked;
+  
+  if (isLiked) {
+    counter.textContent = 'Liked';
+    likeButton.classList.add('liked');
+  } else {
+    counter.textContent = '';
+    likeButton.classList.remove('liked');
+  }
 }
 

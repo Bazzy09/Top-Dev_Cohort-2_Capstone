37e571050c71105d207ee81347dee20cee855459
joshua nathaniel <ministerjoe456@gmail.com>
2023-11-24 14:11:30 +0100

    add reservation class and initialize reservationStore array.

---
+++ src/reservation.js
@@ -0,0 +1,9 @@
+let reservationStore = [];
+
+class Reservation {
+  constructor(name, startDate, endDate) {
+    this.name = name;
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+}
